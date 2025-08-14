5c725bc115f23cd2993901c64648ed1bd020e3c6
SelmanSaracoglu <selman.saracoglu@outlook.com>
2025-08-15 01:04:27 +0200

    ✨ Add hero banner and FAQ section to HomePage for improved pre-login UX

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomePage } from './pages/home/home.page';
 
 
 export const routes: Routes = [
-  { path: '',        loadComponent: ()      => import('./pages/home/home.page').then(m => m.HomePage) },
-  { path: 'animals', loadComponent: ()      => import('./pages/animals/animals.page').then(m => m.AnimalsPage) },
-  { path: 'animals/:id', loadComponent: ()  =>import('./pages/animals/animal-detail.page/animal-detail.page').then(m => m.AnimalDetailPage), data: { title: 'Animal' } },
-  { path: 'staff',   loadComponent: ()      => import('./pages/staff/staff.page').then(m => m.StaffPage) },
+  { path: '',             loadComponent: ()       =>  import('./pages/home/home.page').then(m => m.HomePage) },
+  { path: 'about',        loadComponent: ()       =>  import('./pages/about/about').then(m => m.About) },
+  { path: 'events',       loadComponent: ()       =>  import('./pages/events/events').then(m => m.Events) },
+  { path: 'programs',     loadComponent: ()       =>  import('./pages/programs/programs').then(m => m.Programs) },
+  { path: 'map',          loadComponent: ()       => import('./pages/map/map').then(m => m.Map),},
+  { path: 'animals',      loadComponent: ()       =>  import('./pages/animals/animals.page').then(m => m.AnimalsPage) },
+  { path: 'animals/:id',  loadComponent: ()       =>  import('./pages/animals/animal-detail.page/animal-detail.page').then(m => m.AnimalDetailPage), data: { title: 'Animal' } },
+  { path: 'staff',        loadComponent: ()       =>  import('./pages/staff/staff.page').then(m => m.StaffPage) },
   { path: '**', redirectTo: '' },
 
 ];
---
+++ src/app/pages/programs/programs.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-programs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './programs.html',
+  styleUrls: ['./programs.scss']
+})
+export class Programs {
+
+}
